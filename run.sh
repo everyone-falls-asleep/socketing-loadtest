ec9ea1adb8445f23e69f460b16646ac11e30e4bf
hjyoon <hjyoon@kakao.com>
2024-12-10 02:37:03 +0900

    refactor: Simplify Locust worker configuration and update run script

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@ CPU_CORES=$(nproc)
 
 echo "Starting Locust cluster with $CPU_CORES workers (based on CPU cores)..."
 echo "Locust cluster is running."
-echo "Master UI available at: http://localhost:8089"
 
 docker-compose up --scale worker="$CPU_CORES"
